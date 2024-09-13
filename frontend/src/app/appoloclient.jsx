5509173ba65555e9f189f59ec0f055568ad42f91
Jishnu-21 <144204571+Jishnu-21@users.noreply.github.com>
2024-09-13 11:44:10 +0530

    Update appoloclient.jsx

--- frontend/src/app/appoloclient.jsx
+++ frontend/src/app/appoloclient.jsx
@@ -2,7 +2,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://novelwriter.onrender.com/graphql', t
+  uri: 'https://novelwriter.onrender.com/graphql', // Replace with your GraphQL endpoint
   cache: new InMemoryCache(),
 });
 
